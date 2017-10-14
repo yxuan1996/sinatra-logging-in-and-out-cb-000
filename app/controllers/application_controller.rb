5f4be6db0fe2b9b1c7467add053425b8d65de407
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-14 19:34:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,24 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+    erb :error
 
   end
 
   get '/account' do
+    erb :account
 
   end
 
   get '/logout' do
+    session.clear
 
   end
 
 
 end
-
